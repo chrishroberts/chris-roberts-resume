0891a7ebd56045ec549e8ea1dc936769cd8f561c
chrishroberts <c.h.roberts33@gmail.com>
2026-04-08 13:53:26 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChrisRobertsSamsaraResume from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChrisRobertsSamsaraResume />
+  </React.StrictMode>
+)
+
